47f6aeb9f834c3168a793e3dceae116964ad2eb0
Seb Shader <sebfumaster@aol.com>
2022-01-03 13:50:48 -0800

    add gruvbox Material Light theme

---
+++ themes/theme-gruvboxMaterialLight-plugin.tcl
@@ -0,0 +1,62 @@
+# theme from https://github.com/Eric-Lennartson/pd-themes
+# gruvbox material light
+array set ::pd_colors {
+    gop_box 		         "#EA6962"
+
+    atom_box_label 	 	     "#928374"
+    comment 		         "#928374"
+
+    obj_box_outline 	     "#f2e5bc"
+    msg_box_outline 	     "#f2e5bc"
+    atom_box_outline 	     "#f2e5bc"
+    msg_box_fill 		     "#f2e5bc"
+    obj_box_fill 		     "#f2e5bc"
+    atom_box_fill 		     "#f2e5bc"
+    atom_box_focus_outline   "#7DAEA3"
+
+    obj_box_text 		     "#6c782e"
+    msg_box_text 		     "#805F4B"
+    atom_box_text 		     "#945e80"
+    pdwindow_post_text 	     "#654735"
+    helpbrowser_text 	     "#654735"
+    text_window_text 	     "#654735"
+
+    signal_cord 		     "#B0C15C"
+    signal_iolet 		     "#B0C15C"
+    signal_iolet_border      "#B0C15C"
+    msg_cord 		         "#A8A29F"
+    msg_iolet 		         "#A8A29F"
+    msg_iolet_border         "#A8A29F"
+
+    graph_outline 		     "#654735"
+    graph_text 		         "#654735"
+    array_name 		         "#654735"
+    array_values 		     "#4C7A5D"
+
+    canvas_fill 		     "#F9F5D7"
+    pdwindow_fill 		     "#F9F5D7"
+    text_window_fill 	     "#F9F5D7"
+    helpbrowser_fill 	     "#F9F5D7"
+
+    obj_box_outline_broken   "#c14a4a"
+    pdwindow_fatal_text 	 "#c14a4a"
+    pdwindow_error_text 	 "#c14a4a"
+    pdwindow_fatal_highlight "#F9F5D7"
+    pdwindow_debug_text 	 "#C35E0A"
+
+    selection_rectangle      "#7DAEA3"
+    selected 		         "#7DAEA3"
+    helpbrowser_highlight 	 "#EBDBB2"
+    helpbrowser_hl_text      "#7DAEA3"
+    text_window_highlight    "#EBDBB2"
+    text_window_hl_text      "#7DAEA3"
+    txt_highlight 		     "#EBDBB2"
+    txt_highlight_front      "#7DAEA3"
+
+    text_window_cursor 	     "#654735"
+    canvas_text_cursor 	     "#654735"
+    cursor                   "#654735"
+
+    scrollbox_fill           "#DCD6C5"
+    text                     "#654735"
+}
