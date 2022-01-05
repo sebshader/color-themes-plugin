a1c501dd9b187874956277d3aab19d3c21e46df4
Seb Shader <sebfumaster@aol.com>
2022-01-04 16:10:49 -0800

    Update pd-extended-theme-plugin.tcl
    use correct graph_outline

--- themes/pd-extended-theme-plugin.tcl
+++ themes/pd-extended-theme-plugin.tcl
@@ -9,6 +9,7 @@ msg_box_fill "#f8f8f6"
 atom_box_outline "#ccc"
 atom_box_fill "#eee"
 
+graph_outline "#777"
 msg_cord "#121"
 signal_cord "#558"
 obj_box_outline_broken "#f00"
@@ -18,3 +19,4 @@ canvas_fill "white"
 set ::pd_colors(signal_iolet_border) $::pd_colors(signal_cord)
 set ::pd_colors(msg_iolet_border) $::pd_colors(msg_cord)
 set ::pd_colors(atom_box_focus_outline) $::pd_colors(atom_box_outline)
+set ::pd_colors(array_name) $::pd_colors(graph_outline)
