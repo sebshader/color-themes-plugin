0a349a4a34a96d0487c82fe47aa92b4f643eafef
Seb Shader <sebfumaster@aol.com>
2022-10-11 02:41:16 -0700

    fix array name in pd-extended theme

--- themes/pd-extended-theme-plugin.tcl
+++ themes/pd-extended-theme-plugin.tcl
@@ -4,7 +4,7 @@ signal_iolet "#558"
 msg_iolet "#fff"
 obj_box_outline "#ccc"
 obj_box_fill "#f6f8f8"
-msg_box_outline "#ccc"
+msg_box_outline "#121"
 msg_box_fill "#f8f8f6"
 atom_box_outline "#ccc"
 atom_box_fill "#eee"
@@ -14,9 +14,7 @@ msg_cord "#121"
 signal_cord "#558"
 obj_box_outline_broken "#f00"
 canvas_fill "white"
-
 }
 set ::pd_colors(signal_iolet_border) $::pd_colors(signal_cord)
 set ::pd_colors(msg_iolet_border) $::pd_colors(msg_cord)
 set ::pd_colors(atom_box_focus_outline) $::pd_colors(atom_box_outline)
-set ::pd_colors(array_name) $::pd_colors(graph_outline)
